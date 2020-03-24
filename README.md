6d1f7303f328fcdd207b3ab2a7463959a4f3fadb
Jayson Wu <wulingjie991@outlook.com>
2020-03-24 22:46:48 +0800

    init project

---
+++ README.md
@@ -0,0 +1,28 @@
+# gentpl-cli
+
+> A Project To Generate Template On Your Own Way Like `create-react-app` | `@vue/cli` | `@angular/cli`
+
+## Installation
+
+```bash
+$ npm i -g gentpl-cli
+```
+
+## All Commands
+
+```bash
+# check gentpl-cli version
+gentpl --version
+
+# init your own project
+gentpl init <template> <project>
+
+# list all templates
+gentpl list
+```
+
+## All Templates
+
+_react-template_
+
+> It Contains Jest | Babel | Eslint | Prettier | Webpack | Node-sass.
---
+++ src/index.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const ora = require('ora')
+const chalk = require('chalk')
+const program = require('commander')
+const inquirer = require('inquirer')
+const handlebars = require('handlebars')
+const logSymbols = require('log-symbols')
+const download = require('download-git-repo')
+
+const templates = require('../config/templates.json')
+
+// -v || --version: check your package version
+program.version('0.0.1')
+
+program
+  .command('init <template> <project>')
+  .description('Get Initial Template')
+  .action((templateName, projectName) => {
+    const spinner = ora('Template Downloading...')
+    spinner.start()
+
+    const { downloadUrl } = templates.find((template) => templateName === template.name)
+    download(downloadUrl, projectName, { clone: true }, (err) => {
+      if (err) {
+        spinner.fail('Template Downloaded Failed')
+        console.log(logSymbols.error, chalk.red('Template Downloaded Failed:' + err))
+        return false
+      }
+
+      spinner.succeed('Template Downloaded Success')
+
+      // Change Your Package Info
+      inquirer
+        .prompt([
+          {
+            type: 'input',
+            name: 'name',
+            message: 'Project Name: '
+          },
+          {
+            type: 'input',
+            name: 'description',
+            message: 'Project Description: '
+          },
+          {
+            type: 'input',
+            name: 'author',
+            message: 'Project Author:'
+          }
+        ])
+        .then((answers) => {
+          const packagePath = `${projectName}/package.json`
+          const packageContent = fs.readFileSync(packagePath, 'utf8')
+          const packageResult = handlebars.compile(packageContent)(answers)
+          fs.writeFileSync(packagePath, packageResult)
+          console.log(logSymbols.success, chalk.yellow('Get Initial Template Success'))
+        })
+    })
+  })
+
+program
+  .command('list')
+  .description('List All Templates')
+  .action(() => {
+    console.log('Template List: ')
+    templates.forEach((template) => {
+      console.log(`${chalk.green(template.name)}: ${chalk.yellowBright(template.description)}`)
+    })
+  })
+
+program.parse(process.argv)
