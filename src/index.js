a6baffad0bf06939c2d51a758bf0335080418150
Jayson Wu <wulingjie991@outlook.com>
2020-03-27 22:30:55 +0800

    update cli operation ui

--- src/index.js
+++ src/index.js
@@ -35,32 +35,37 @@ const table = new Table({
 program.version('0.0.1')
 
 program
-  .command('new <app-name>')
+  .command('new <projectName>')
   .description('Create Project Through Exist Template')
   .action((projectName) => {
     let templateName = ''
+    let templateNames = []
+
+    templates.forEach((template) => {
+      templateNames.push(template.name)
+    })
 
     inquirer
       .prompt([
         {
-          type: 'rawlist',
+          type: 'list',
           name: 'name',
-          message: 'Choose A Template: ',
-          choices: [ 'react-template', 'react-typescript-template' ]
+          message: 'Choose A Template That You Want',
+          choices: templateNames
         }
       ])
       .then((option) => {
         templateName = option.name
 
-        const spinner = ora('Template Downloads ...')
+        const spinner = ora('Template Is Downloading')
         spinner.start()
 
         const { downloadUrl } = templates.find((template) => templateName === template.name)
         download(downloadUrl, projectName, { clone: true }, (err) => {
           if (err) {
             spinner.fail('Template Downloads Failed')
-            console.log(logSymbols.error, chalk.red('Template Downloaded Failed:' + err))
-            return false
+            console.log(logSymbols.error, chalk.red('Template Downloaded Failed'))
+            process.exit(0)
           }
 
           spinner.succeed('Template Downloads Success')
@@ -91,7 +96,15 @@ program
               fs.writeFileSync(packagePath, packageResult)
               console.log(logSymbols.success, chalk.yellow('Get Initial Template Success'))
             })
+            .catch((err) => {
+              console.log(logSymbols.error, chalk.red('Get Initial Template Failed: ' + err))
+              process.exit(0)
+            })
         })
+      })
+      .catch((err) => {
+        console.log(logSymbols.error, chalk.red('Create New Project Failed: ' + err))
+        process.exit(0)
       })
   })
 
